010df16eaf27015d6b4c2b9da0dfc4df00c01819
tabarino <tabarino@outlook.com>
2020-07-02 23:08:42 +0100

    Add Firebase to the Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        AngularFirestoreModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyA5miDpUL0ujli7A2TJbHTEYwjsidToSVU',
+        authDomain: 'organic-shop-481ac.firebaseapp.com',
+        databaseURL: 'https://organic-shop-481ac.firebaseio.com',
+        projectId: 'organic-shop-481ac',
+        storageBucket: 'organic-shop-481ac.appspot.com',
+        messagingSenderId: '85744636750',
+        appId: '1:85744636750:web:5c60a0e81c02022689041f',
+        measurementId: 'G-MSC4B9MGR7'
+    }
 };
 
 /*
