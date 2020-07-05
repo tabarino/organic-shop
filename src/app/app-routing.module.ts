552e2c2d8412cafc9382a5664511e26184553172
tabarino <tabarino@outlook.com>
2020-07-05 10:19:54 +0100

    284. Fixing a Few Minor Issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
@@ -16,6 +17,7 @@ const routes: Routes = [
     { path: 'check-out', component: CheckOutComponent },
     { path: 'order-success', component: OrderSuccessComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'my/orders', component: MyOrdersComponent },
     { path: 'admin/products', component: AdminProductsComponent },
     { path: 'admin/orders', component: AdminOrdersComponent },
 ];
