c183f83ab25adb93f273726359bed6bd1faa4809
tabarino <tabarino@outlook.com>
2020-07-09 22:13:24 +0100

    289. Displaying the Current User

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
     selector: 'bs-navbar',
@@ -7,7 +8,10 @@ import { AngularFireAuth } from '@angular/fire/auth';
     styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent implements OnInit {
+    user: firebase.User;
+
     constructor(private afAuth: AngularFireAuth) {
+        this.afAuth.authState.subscribe(user => this.user = user);
     }
 
     ngOnInit(): void {
