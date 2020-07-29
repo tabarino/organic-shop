09e044860ab05153cb93efb19a01b75a3a922e77
tabarino <tabarino@outlook.com>
2020-07-29 19:10:35 +0100

    321. Filtering Products by Category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-products',
@@ -11,17 +12,30 @@ import { Category } from '../models/category';
     styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-    products$: Observable<Product[]>;
+    products: Product[] = [];
+    filteredProducts: Product[] = [];
     categories$: Observable<Category[]>;
+    categoryId: string;
 
     constructor(
+        private route: ActivatedRoute,
         private productService: ProductService,
         private categoryService: CategoryService
     ) {
     }
 
     ngOnInit(): void {
-        this.products$ = this.productService.getAll();
         this.categories$ = this.categoryService.getAll();
+        this.productService.getAll().subscribe(products => this.products = products);
+
+        this.route.queryParamMap.subscribe(params => {
+            this.categoryId = params.get('categoryId');
+
+            if (this.categoryId) {
+                this.filteredProducts = this.products.filter(products => products.category === this.categoryId);
+            } else {
+                this.filteredProducts = this.products;
+            }
+        });
     }
 }
