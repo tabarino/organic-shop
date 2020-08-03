60aeb03b789dc2166f66cd80e94abb748840a34b
tabarino <tabarino@outlook.com>
2020-08-03 22:48:10 +0100

    336. Displaying the Number of Shopping Cart Items in NavBar

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../services/shopping-cart.service';
 
 @Component({
     selector: 'bs-navbar',
@@ -9,12 +10,20 @@ import { AppUser } from '../models/app-user';
 })
 export class BsNavbarComponent implements OnInit {
     appUser: AppUser;
+    shoppingCartItemCount: number;
 
-    constructor(private authService: AuthService) {
-        this.authService.appUser$.subscribe(appUser => this.appUser = appUser);
+    constructor(private authService: AuthService, private cartService: ShoppingCartService) {
     }
 
-    ngOnInit(): void {
+    async ngOnInit(): Promise<void> {
+        this.authService.appUser$.subscribe(appUser => this.appUser = appUser);
+        const cart$ = await this.cartService.getCart();
+        cart$.subscribe(shoppingCart => {
+            this.shoppingCartItemCount = 0;
+            for (const item of shoppingCart) {
+                this.shoppingCartItemCount += item.quantity;
+            }
+        });
     }
 
     logout(): void {
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,4 +1,7 @@
+import { ShoppingCartItem } from './shopping-cart-item';
+
 export interface ShoppingCart {
     id: string;
     dateCreated: number;
+    items: ShoppingCartItem[];
 }
