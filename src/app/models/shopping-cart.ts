0a9b63185295834ba90cd6a9d606378477389915
tabarino <tabarino@outlook.com>
2020-08-04 23:13:56 +0100

    340. Displaying the Total Price

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,7 +1,12 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart {
-    constructor(public items: ShoppingCartItem[]) {
+    items: ShoppingCartItem[] = [];
+
+    constructor(public itemsMap: ShoppingCartItem[]) {
+        for (const item of itemsMap) {
+            this.items.push(new ShoppingCartItem(item.product, item.quantity));
+        }
     }
 
     get totalItemsCount(): number {
@@ -10,5 +15,13 @@ export class ShoppingCart {
             count += item.quantity;
         }
         return count;
+    }
+
+    get totalPrice(): number {
+        let sum = 0;
+        for (const item of this.items) {
+            sum += item.totalPrice;
+        }
+        return sum;
     }
 }
