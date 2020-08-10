106d5639e30eb3d971e18f2100b2f628222eb778
tabarino <tabarino@outlook.com>
2020-08-10 21:49:21 +0100

    359. Adding an Order Summary Widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { CustomFormsModule } from 'ng2-validation';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
+import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { ProductQuantityComponent } from './product-quantity/product-quantity.co
         ProductFormComponent,
         ProductFilterComponent,
         ProductCardComponent,
-        ProductQuantityComponent
+        ProductQuantityComponent,
+        ShoppingCartSummaryComponent
     ],
     imports: [
         BrowserModule,
