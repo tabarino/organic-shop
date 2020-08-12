4e391efefc03eace5155a54801673e9f209f306c
tabarino <tabarino@outlook.com>
2020-08-12 23:40:55 +0100

    367. Moving Components to Core Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { ProductsComponent } from '@shopping/components/products/products.compon
 import { ShoppingCartComponent } from '@shopping/components/shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from '@shopping/components/check-out/check-out.component';
 import { OrderSuccessComponent } from '@shopping/components/order-success/order-success.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from '@core/components/login/login.component';
 import { MyOrdersComponent } from '@shopping/components/my-orders/my-orders.component';
 import { AdminProductsComponent } from '@admin/components/admin-products/admin-products.component';
 import { AdminOrdersComponent } from '@admin/components/admin-orders/admin-orders.component';
--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@shared/services/auth.service';
 
 @Component({
-    selector: 'app-login',
+    selector: 'login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
