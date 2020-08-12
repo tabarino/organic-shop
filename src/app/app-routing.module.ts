5b95e19f032bca9c53f79f89441636671f9f9a13
tabarino <tabarino@outlook.com>
2020-08-12 23:26:03 +0100

    367. Moving Components and Services to Admin Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { LoginComponent } from './login/login.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from '@admin/components/admin-products/admin-products.component';
+import { AdminOrdersComponent } from '@admin/components/admin-orders/admin-orders.component';
 import { AuthGuardService } from '@shared/services/auth-guard.service';
-import { AdminAuthGuardService } from './services/admin-auth-guard.service';
-import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { AdminAuthGuardService } from '@admin/services/admin-auth-guard.service';
+import { ProductFormComponent } from '@admin/components/product-form/product-form.component';
 
 const routes: Routes = [
     { path: '', component: ProductsComponent },
